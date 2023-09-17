64650388e6b3816c12ce9b455b6e7aff414e1237
LeonelSOliveira <117734199+LeonelSOliveira@users.noreply.github.com>
2023-09-16 22:06:45 -0300

    Update voz.js

--- voz.js
+++ voz.js
@@ -1,14 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Inicializando o reconhecimento de voz
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    if(!SpeechRecognition) {
-        console.error("O navegador não suporta a API de reconhecimento de voz.");
-        return;
-    }
     
-    const recognition = new SpeechRecognition();
-    recognition.lang = 'pt-BR'; // Define o idioma para Português do Brasil
-
     // Elementos da interface
     const voiceButton = document.getElementById("voice-button");
     const noteInput = document.getElementById("note-input");
@@ -17,19 +10,38 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error("Elementos necessários para a transcrição de voz não encontrados.");
         return;
     }
+    
+    if (!SpeechRecognition) {
+        console.error("O navegador não suporta a API de reconhecimento de voz.");
+        voiceButton.style.opacity = "0.2";  // Torna o botão mais transparente se a API não estiver disponível
+        return;
+    }
+
+    let isRecording = false;  // Flag para verificar se está gravando
+    
+    const recognition = new SpeechRecognition();
+    recognition.lang = 'pt-BR'; // Define o idioma para Português do Brasil
 
     // Eventos para o botão de voz
-    voiceButton.addEventListener("click", function () {
-        recognition.start();
+    voiceButton.addEventListener("mousedown", function () {
+        isRecording = true;
+        recognition.start();  // Inicia o reconhecimento de voz
+    });
+
+    voiceButton.addEventListener("mouseup", function () {
+        isRecording = false;
+        recognition.stop();  // Finaliza o reconhecimento de voz
     });
 
     // Eventos para o reconhecimento de voz
     recognition.addEventListener("result", function (event) {
-        const transcript = Array.from(event.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join("");
-        noteInput.value = transcript; // Insere o texto reconhecido no campo de entrada
+        if (isRecording) {
+            const transcript = Array.from(event.results)
+                .map(result => result[0])
+                .map(result => result.transcript)
+                .join("");
+            noteInput.value = transcript; // Insere o texto reconhecido no campo de entrada
+        }
     });
 
     // Evento para tratamento de erros
